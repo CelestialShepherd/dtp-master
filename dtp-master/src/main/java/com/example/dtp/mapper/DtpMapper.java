1b2a1175aa0d5dd592f3730979264364dd116e1f
Alex <alexdob925@gmail.com>
2022-02-09 16:07:03 +0300

    ...

--- dtp-master/src/main/java/com/example/dtp/mapper/DtpMapper.java
+++ dtp-master/src/main/java/com/example/dtp/mapper/DtpMapper.java
@@ -3,15 +3,17 @@ package com.example.dtp.mapper;
 import com.example.dtp.dto.DtpDto;
 import com.example.dtp.entity.DtpEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import java.util.Collection;
 import java.util.List;
 
-@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_DEFAULT)
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface DtpMapper {
     List<DtpDto> toDtpDtoList(Collection<DtpEntity> dtpEntities);
+    @Mapping(target = "location_id", ignore = true)
     DtpEntity toDtpEntity(DtpDto dtpDto);
     DtpDto toDtpDto(DtpEntity dtpEntity);
    // DtpEntity updateFromDto(DtpDto source, @MappingTarget DtpEntity target);
